d0843f2efd78c52397efceb403379fb4079488d6
Mathias Dragovic <dragovic.mathias@gmail.com>
2022-11-13 18:01:16 +0100

    start of mocked data

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,11 +1,20 @@
+import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE} from '../API/data';
 import axios from "axios";
 
 const BASE_URL = 'http://localhost:3000';
 
+let mockedData = true;
+
 export async function getUserInfos(userId) {
     try {
-        const { data } = await axios.get(`${BASE_URL}/user/${userId}`);
-        return data;
+        if (!mockedData) {
+            const { data } = await axios.get(`${BASE_URL}/user/${userId}`);
+            return data;
+        } else {
+            const mockedData = USER_MAIN_DATA;
+            console.log(mockedData);
+            return mockedData;
+        }  
     } catch (error) {
         console.log(error);
     }
